c24ed6701382f47cb92cdb19ea54efef2793fd9f
Francisco Moretti <franciscoemoretti@gmail.com>
2023-10-28 20:51:48 +0100

    convert epub to md

--- api/index.py
+++ api/index.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
 import pypandoc
 from bs4 import BeautifulSoup
+import tempfile
+import os
 
 app = Flask(__name__)
 
@@ -17,15 +19,25 @@ def compare_files():
         file2 = request.files['file2']
 
         if file1 and file2:
-            file1_content = file1.read().decode('utf-8')
-            file2_content = file2.read().decode('utf-8')
+            # file1_content = file1.read().decode('utf-8')
+            # file2_content = file2.read().decode('utf-8')
 
-            soup = BeautifulSoup(file1_content, "html.parser")
+            with tempfile.NamedTemporaryFile(delete=False, suffix=".epub") as temp_file1:
+                file1.save(temp_file1.name)
+                article_epub_file_path = temp_file1.name
 
-            common_text = pypandoc.convert_text(
-                source=soup, format="html", to="gfm"
-            )
-            
+                output = pypandoc.convert_file(
+                    source_file=article_epub_file_path,
+                    format="epub",
+                    to="html",
+                )
+
+                soup = BeautifulSoup(output, "html.parser")
+
+                common_text = pypandoc.convert_text(
+                    source=soup, format="html", to="gfm"
+                )
+
 
             response = {
                 "common_text": common_text
@@ -36,3 +48,6 @@ def compare_files():
         return jsonify({"error": "File upload failed."}), 400
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+    finally:
+        # Clean up temporary files
+        os.unlink(article_epub_file_path)
