66ac73c2e48d0f75d6244fc047cb2de20e650f21
Francisco Moretti <franciscoemoretti@gmail.com>
2023-10-28 18:31:54 +0100

    demo app

--- api/index.py
+++ api/index.py
@@ -1,6 +1,38 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import pypandoc
+from bs4 import BeautifulSoup
+
 app = Flask(__name__)
 
 @app.route("/api/python")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+
+@app.route('/api/compare-files', methods=['POST'])
+def compare_files():
+    try:
+        file1 = request.files['file1']
+        file2 = request.files['file2']
+
+        if file1 and file2:
+            file1_content = file1.read().decode('utf-8')
+            file2_content = file2.read().decode('utf-8')
+
+            soup = BeautifulSoup(file1_content, "html.parser")
+
+            common_text = pypandoc.convert_text(
+                source=soup, format="html", to="gfm"
+            )
+            
+
+            response = {
+                "common_text": common_text
+            }
+
+            return jsonify(response)
+
+        return jsonify({"error": "File upload failed."}), 400
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
